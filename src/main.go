eb346b2ac1bb3268fe2f49f0fd13b790bab85ce9
marjune <marjune@163.com>
2025-02-04 19:06:08 +0800

    feat: update API calls

--- src/main.go
+++ src/main.go
@@ -1,50 +1,15 @@
 package src
 
 import (
-	"errors"
 	localDefaultTheme "mjpclab.dev/ehfs/src/defaultTheme"
 	"mjpclab.dev/ehfs/src/middleware"
 	"mjpclab.dev/ehfs/src/param"
 	"mjpclab.dev/ehfs/src/version"
 	"mjpclab.dev/ghfs/src"
-	"mjpclab.dev/ghfs/src/app"
 	"mjpclab.dev/ghfs/src/serverError"
 	"mjpclab.dev/ghfs/src/setting"
 	"mjpclab.dev/ghfs/src/tpl/defaultTheme"
-	"os"
-	"os/signal"
-	"syscall"
 )
-
-func cleanupOnEnd(appInst *app.App) {
-	chSignal := make(chan os.Signal)
-	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		<-chSignal
-		appInst.Shutdown()
-	}()
-}
-
-func reInitOnHup(appInst *app.App) {
-	chSignal := make(chan os.Signal)
-	signal.Notify(chSignal, syscall.SIGHUP)
-
-	go func() {
-		for range chSignal {
-			errs := appInst.ReOpenLog()
-			if serverError.CheckError(errs...) {
-				appInst.Shutdown()
-				break
-			}
-			errs = appInst.ReLoadCertificates()
-			if serverError.CheckError(errs...) {
-				appInst.Shutdown()
-				break
-			}
-		}
-	}()
-}
 
 func Main() (ok bool) {
 	// params
@@ -73,28 +38,8 @@ func Main() (ok bool) {
 	// settings
 	settings := setting.ParseFromEnv()
 
-	// CPU profile
-	if len(settings.CPUProfileFile) > 0 {
-		cpuProfileFile, err := src.StartCPUProfile(settings.CPUProfileFile)
-		if serverError.CheckError(err) {
-			return
-		}
-		defer src.StopCPUProfile(cpuProfileFile)
-	}
-
-	// app
-	appInst, errs := app.NewApp(baseParams, settings)
-	if serverError.CheckError(errs...) {
-		return
-	}
-	if appInst == nil {
-		serverError.CheckError(errors.New("failed to create application instance"))
-		return
-	}
-
-	cleanupOnEnd(appInst)
-	reInitOnHup(appInst)
-	errs = appInst.Open()
+	// start
+	errs = src.Start(settings, baseParams)
 	if serverError.CheckError(errs...) {
 		return
 	}
