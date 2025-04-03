121e5e654c310a02f853631f9c85785e6f33e459
marjune <marjune@163.com>
2025-04-03 20:31:04 +0800

    chore(build): generate TGZ archive for UNIX-like system
    - UNIX-like system: use TGZ archive format
    - Windows system: continue using ZIP format

--- build/build.inc.sh
+++ build/build.inc.sh
@@ -1,12 +1,10 @@
 export GOPROXY=https://goproxy.cn,direct
 export CGO_ENABLED=0
-TMP='/tmp'
 OUTDIR='../output'
 MAINNAME='ehfs'
 MOD=$(go list ../src/)
 BASEMOD=mjpclab.dev/ghfs/src
 source ./build.inc.version.sh
-LICENSE='../LICENSE'
 getLdFlags() {
 	echo "-s -w -X $BASEMOD/version.appVer=$VERSION -X $BASEMOD/version.appArch=${ARCH:-$(go env GOARCH)}"
 }
